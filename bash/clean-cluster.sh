e2fd899be68f9aa6d4b648539be159414961eec7
Manuel Morejon <manuel@mmorejon.io>
2020-04-01 21:42:51 +0200

    Add clean script.
    Signed-off-by: Manuel Morejon <manuel@mmorejon.io>

---
+++ bash/clean-cluster.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# base dir
+BASEDIR='/home/vagrant/erase-una-vez-k8s'
+COMMON_FLAGS='--ignore-not-found=true'
+
+printf 'Iniciando el proceso de limpieza en el cluster ... \n'
+
+# clean objets from chapter pods
+kubectl --namespace default delete -f $BASEDIR/pods/ $COMMON_FLAGS
+kubectl --namespace default delete -f $BASEDIR/labels/ $COMMON_FLAGS
+kubectl --namespace default delete -f $BASEDIR/replicasets/ $COMMON_FLAGS
+kubectl --namespace default delete -f $BASEDIR/deployments/ $COMMON_FLAGS
+kubectl --namespace default delete -f $BASEDIR/services/ $COMMON_FLAGS
+kubectl --namespace default delete -f $BASEDIR/ingress/ $COMMON_FLAGS
+kubectl delete -f $BASEDIR/ingress/nginx/ $COMMON_FLAGS
+kubectl delete -f $BASEDIR/namespaces/ $COMMON_FLAGS
+
+printf '\nHa terminado el proceso de limpieza en el cluster.\n'
