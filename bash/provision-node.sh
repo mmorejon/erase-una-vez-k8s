b1dd1ec0ec50579216e242d8d1bc15cceacdfa92
Manuel Morejon <manuel@mmorejon.io>
2023-04-12 22:23:51 +0200

    feat(vagrant): remove old files used by vagrant
    Signed-off-by: Manuel Morejon <manuel@mmorejon.io>

--- bash/provision-node.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# add nodes to kubernetes cluster
-envsubst < ${REPO_PATH}/cluster/default-node.yaml > ${REPO_PATH}/cluster/node.yaml
-kubeadm join --config ${REPO_PATH}/cluster/node.yaml
-rm ${REPO_PATH}/cluster/node.yaml
