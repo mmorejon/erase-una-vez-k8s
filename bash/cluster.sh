f99092997ae7f8b7f341ddb352cad42265a4fef7
Manuel Morejon <manuel@mmorejon.io>
2025-01-12 23:18:25 +0100

    add cluster multi-region and deploy-5 files
    Signed-off-by: Manuel Morejon <manuel@mmorejon.io>

--- bash/cluster.sh
+++ bash/cluster.sh
@@ -8,6 +8,11 @@ if [[ " ${args[*]} " == *" create "* ]]; then
 kind create cluster \
     --config=cluster/kind-config.yaml
 fi
+# cluster multi-region
+if [[ " ${args[*]} " == *" create-multi-region "* ]]; then
+kind create cluster \
+    --config=cluster/multi-region.yaml
+fi
 # delete cluster
 if [[ " ${args[*]} " == *" delete "* ]]; then
 kind delete cluster --name book
@@ -16,6 +21,11 @@ fi
 if [[ " ${args[*]} " == *" restart "* ]]; then
 ./bash/cluster.sh delete
 ./bash/cluster.sh create
+fi
+# restart cluster multi-region
+if [[ " ${args[*]} " == *" restart-multi-region "* ]]; then
+./bash/cluster.sh delete
+./bash/cluster.sh create-multi-region
 fi
 # clean cluster
 if [[ " ${args[*]} " == *" clean "* ]]; then
